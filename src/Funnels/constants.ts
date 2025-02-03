5acc1db22c556cd369502677341bcf0cf6432dc3
xtring <hh940630@gmail.com>
2025-02-03 23:47:54 +0900

    modify: funnel title 수정

--- src/Funnels/constants.ts
+++ src/Funnels/constants.ts
@@ -22,13 +22,13 @@ const QUESTION_STEP_LIST = Object.keys(QUESTION_STEPS) as QuestionStepType[];
 
 const CONTENT_BY_STEP: Record<QuestionStepType, Content> = {
   [QUESTION_STEPS.INTRO]: {
-    title: "안녕하세요!\n저는 당신의 목표를 이루는데 도움을 줄 Pomodori입니다.",
+    title: "안녕하세요.\n저는 당신의 목표를 이루는데 도움을 줄 Pomodori입니다.",
   },
   [QUESTION_STEPS.NAME]: {
-    title: "이름이 뭐에요?",
+    title: "이름이 어떻게되세요?",
   },
   [QUESTION_STEPS.GOAL]: {
-    title: "목표가 있으세요?",
+    title: "목표가 있으실까요?\n되도록 큰 범위의 목표를 생각해보세요.",
   },
   [QUESTION_STEPS.FINISH]: {
     title: "지금 잘하고 있어요!\n앞으로도 계속해봐요.",
